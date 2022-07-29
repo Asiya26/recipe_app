69bc6565f028f606d372538c121f37dbd2502386
Your Name <Your email>
2022-07-29 02:05:22 -0700

    first part

--- src/components/home.js
+++ src/components/home.js
@@ -7,10 +7,20 @@ import {
   CardBody,
   CardTitle,
   Button,
+  Input,
 } from "reactstrap";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 function Home() {
   const [userData, setData] = useState([]);
+  const [searchText, setSearchText] = useState("avocado");
+  const [mealType, setMealType] = useState("breakfast");
+
+  const [startSearching, setStarSearching] = useState(false);
   const fetchingData = (url) => {
     fetch(url)
       .then((res) => res.json())
@@ -20,17 +30,56 @@ function Home() {
         setData(data.hits);
       });
   };
+  const handleChange = (e) => {
+    setSearchText(e.target.value);
+  };
+  const handleSerch = () => {
+    setStarSearching(!startSearching);
+  };
+
+  const handleChangeMealType = (event) => {
+    setMealType(event.target.value);
+  };
 
   useEffect(() => {
     const apiID = "dd95e753";
     const apiKey = "460370faacc836991a9ad941680bb41d";
-    const url = `https://api.edamam.com/search?q=avocado&app_id=${apiID}&app_key=${apiKey}&mealType=lunch`;
+    const url = `https://api.edamam.com/search?q=${searchText}&app_id=${apiID}&app_key=${apiKey}&mealType=${mealType}`;
     fetchingData(url);
-  }, []);
+  }, [startSearching]);
 
   return (
     <>
-      <div className="row">
+      <div className="row mt-5">
+        <div className="col-12 col-sm-5">
+          <div className="d-flex">
+            <Input onChange={handleChange} value={searchText} />
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Meal Type</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={mealType}
+                  label="mealType"
+                  onChange={handleChangeMealType}
+                >
+                  <MenuItem value='breakfast'>Breakfast</MenuItem>
+                  <MenuItem value='lunch'>Lunch</MenuItem>
+                  <MenuItem value='dinner'>Dinner</MenuItem>
+                  <MenuItem value='snack'>Snack</MenuItem>
+
+                </Select>
+              </FormControl>
+            </Box>
+
+            <Button color="primary" onClick={handleSerch}>
+              Search
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="row mt-5">
         {userData.length ? (
           userData.map((item) => {
             return (
@@ -44,10 +93,10 @@ function Home() {
                   />
                   <CardBody>
                     <CardTitle tag="h5">
-                      {item.recipe.lable.slice(0, 20)}...
+                      {item.recipe.label.slice(0, 20)}...
                     </CardTitle>
 
-                    <Button>Button</Button>
+                    <Button>More...</Button>
                   </CardBody>
                 </Card>
               </div>
